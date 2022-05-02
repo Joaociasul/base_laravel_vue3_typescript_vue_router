15fa6e64b447441e8107922f86392f616c9115a2
joaociasul <joaocarlosroyer2018@gmail.com>
2022-05-01 21:14:43 -0300

    first commit

---
+++ resources/js/router.ts
@@ -0,0 +1,28 @@
+import {
+    createRouter,
+    createWebHistory,
+    RouteRecordRaw,
+  } from "vue-router";
+
+  import Home from "./vue/views/Home.vue";
+  import About from "./vue/views/About.vue";
+
+  const routes: Array<RouteRecordRaw> = [
+    {
+      path: "/",
+      name: "Home page",
+      component: Home,
+    },
+    {
+        path: "/about",
+        name: "About page",
+        component: About,
+      }
+  ];
+
+  const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
+  });
+
+  export default router;
---
+++ resources/js/vue/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1 class="text-3xl">{{ hello }}</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+export default defineComponent({
+    setup() {
+        const hello = ref<String>("hello");
+        const elemento = document.querySelector("#teste");
+        if (elemento) {
+            elemento.localName;
+        }
+        return {
+            hello,
+        };
+    },
+});
+</script>
+
+<style lang="scss"></style>
